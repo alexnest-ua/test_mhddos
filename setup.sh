4ff304bf3746b099a4708b961613b561ac4c4119
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-03-19 22:56:59 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-# Install git, python3, pip if doesn't installed already
-apt update -y
-if [ ! -f /usr/bin/git ]; then
-   sudo apt install git -y
-fi
-if [ ! -f /usr/bin/python3 ]; then
-   sudo apt install python3 -y
-fi
-if [ ! -f /usr/bin/pip ]; then
-   apt install python3-pip  -y
-fi
-if [ ! -f /usr/bin/wget ]; then
-   apt install wget  -y
-fi
-if [ ! -f /usr/bin/curl ]; then
-   apt install curl  -y
-fi
-pip install --upgrade pip > /dev/null #No output. Resolved some problems with pip on debian
+
 
 #Install latest version of mhddos_proxy and MHDDoS
 cd ~
